4e27558ff4a61574c29de3d9e43d9fda8b04603c
bautistagorchs <bautista.gorchs@gmail.com>
2024-04-01 10:32:11 -0300

    remove jest from project

--- test/app.testing.spec.tsx
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-describe("probando los test", () => {
-  test("tiene que ir bien", () => {
-    expect(true).toStrictEqual(true);
-  });
-});
